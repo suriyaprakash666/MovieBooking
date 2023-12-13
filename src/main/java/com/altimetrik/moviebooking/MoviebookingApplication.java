f7e1f8446df1ee3576f77ad2f10551c56397e565
Mangnale Manisha Sopanrao <mmanisha@altimetrik.com>
2023-12-13 17:46:38 +0530

    Manisha updated commit

--- src/main/java/com/altimetrik/moviebooking/MoviebookingApplication.java
+++ src/main/java/com/altimetrik/moviebooking/MoviebookingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoviebookingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MoviebookingApplication.class, args);
 	}
 
--- src/main/java/com/altimetrik/moviebooking/repository/SeatsRepository.java
+++ src/main/java/com/altimetrik/moviebooking/repository/SeatsRepository.java
@@ -3,5 +3,8 @@ package com.altimetrik.moviebooking.repository;
 import com.altimetrik.moviebooking.entity.Seats;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatsRepository extends JpaRepository<Seats,Long> {
+
 }
