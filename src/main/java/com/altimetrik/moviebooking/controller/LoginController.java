ab6f5cb0c1e9dd98ee4d8ef4d0360a5463a8c155
Dipak Ashok Sinkar <dsinkar@altimetrik.com>
2023-12-14 09:42:42 +0530

    login feauture changes

--- src/main/java/com/altimetrik/moviebooking/controller/LoginController.java
+++ src/main/java/com/altimetrik/moviebooking/controller/LoginController.java
@@ -1,4 +1,22 @@
 package com.altimetrik.moviebooking.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
+    @Autowired
+
+
+    @PostMapping("/login")
+    public String login() {
+
+    return "Login Successful";
+    }
 }
