18faf75431948ace1fc1fb86ef6193702f043e89
Rehna J <jrehna@altimetrik.com>
2023-12-14 10:16:42 +0530

    rehna updated message

--- src/main/java/com/altimetrik/moviebooking/entity/Cinema.java
+++ src/main/java/com/altimetrik/moviebooking/entity/Cinema.java
@@ -1,7 +1,11 @@
 package com.altimetrik.moviebooking.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +16,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Cinema {
     @Id
-    private int cinemaId;
+    @GeneratedValue
+    private long cinemaId;
     private String cinemaName;
     private String cinemaLocation;
     private int cinemaScreens;
     private long capacity;
-    private String facilities;
+
 }
--- src/main/java/com/altimetrik/moviebooking/service/ICinemaService.java
+++ src/main/java/com/altimetrik/moviebooking/service/ICinemaService.java
@@ -1,5 +1,18 @@
 package com.altimetrik.moviebooking.service;
 
-public interface CinemaSerciveInterface {
+import com.altimetrik.moviebooking.entity.Cinema;
+import com.altimetrik.moviebooking.exception.CinemaIdNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICinemaService {
+    public Cinema saveCinema(Cinema cinema);
+    public List<Cinema> getAllCinemas();
+    public Cinema updateCinema(Cinema cinema);
+    public Optional<Cinema> deleteCinema(long cinemaId);
+    public Cinema getCinemaById(long cinemaId) throws CinemaIdNotFoundException;
+    public List<Cinema>getCinemaByLocation(String cinemaLocation);
+
 
 }
